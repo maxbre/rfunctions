a9d5cc57fdec5c3ef5d0225eb7c13fcde601b723
max <maxbre67@gmail.com>
2022-11-07 18:41:03 +0100

    new fuctions for wind conversion

--- R/wd_convert.R
+++ R/wd_convert.R
@@ -1,5 +1,5 @@
 #' wind_degr_to_sect
-#' 
+#'
 #' convert wind degrees to sectors
 #'
 #' @param vd a numeric vector of wind dir degrees
@@ -39,7 +39,7 @@ wind_degr_to_sect<-function(vd){
 #' wind_sect_to_degr
 #'
 #' convert wind sectors to degrees
-#' 
+#'
 #' @param vs a character vector of wind dir sectors
 #' @return a numeric vector of wind dir degrees
 #' @export
@@ -72,4 +72,92 @@ wind_sect_to_degr<-function(vs){
 
 }
 
+#' convert wind direction and wind speed to u and v components
+#'
+#' @description
+#' this function converts wind direction and wind speed to u and v components
+#'
+#' @param wd a vector of wind directions
+#' @param ws a vector of wind speeds
+#'
+#' @return
+#' a matrix
+#'
+#' @seealso \code{\link{uv2wdws}}
+#'
+#' @author
+#' Tim Appelhans
+#' https://raw.githubusercontent.com/environmentalinformatics-marburg/Rsenal/master/R/wdws2uv.R
+#'
+#' @examples
+#' set.seed(123)
+#' wd <- as.integer(rnorm(10, 180, 90))
+#'
+#' set.seed(123)
+#' ws <- rnorm(10, 4, 1)
+#'
+#' ## convert to u and v
+#' wdws2uv(wd, ws)
+#'
+#' ## convert back
+#' uv <- wdws2uv(wd, ws)
+#' uv2wdws(uv[, 1], uv[, 2])
+#'
+#' @export wdws2uv
+#' @aliases wdws2uv
 
+wdws2uv <- function(wd, ws) {
+
+  radians <- function(degrees) degrees * pi / 180
+  u <- -ws * sin(radians(wd))
+  v <- -ws * cos(radians(wd))
+  return(cbind(u, v))
+
+}
+
+#' convert u and v components to wind direction and wind speed
+#'
+#' @description
+#' this function converts u and v components to wind direction and wind speed
+#'
+#' @param u a vector of u components
+#' @param v a vector of v components
+#'
+#' @return
+#' a matrix
+#'
+#' @seealso \code{\link{wdws2uv}}
+#'
+#' @author
+#' Tim Appelhans
+#' https://raw.githubusercontent.com/environmentalinformatics-marburg/Rsenal/master/R/uv2wdws.R
+#'
+#' @examples
+#' set.seed(123)
+#' wd <- as.integer(rnorm(10, 180, 90))
+#'
+#' set.seed(123)
+#' ws <- rnorm(10, 4, 1)
+#'
+#' ## convert to u and v
+#' wdws2uv(wd, ws)
+#'
+#' ## convert back
+#' uv <- wdws2uv(wd, ws)
+#' uv2wdws(uv[, 1], uv[, 2])
+#'
+#' @export uv2wdws
+#' @aliases uv2wdws
+
+uv2wdws <- function(u,v) {
+
+  degrees <- function(radians) 180 * radians / pi
+
+  mathdegs <- degrees(atan2(v, u))
+  wdcalc <- ifelse(mathdegs > 0, mathdegs, mathdegs + 360)
+  wd <- ifelse(wdcalc < 270, 270 - wdcalc, 270 - wdcalc + 360)
+  ws <- sqrt(u^2 + v^2)
+
+  return(cbind(wd, ws))
+
+}
